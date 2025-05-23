2bb1874c95a665a08e3abd71c9a3ec8191bb16f4
stevebiczyk <stevebiczyk@gmail.com>
2025-05-23 23:17:30 +0100

    defined custom user, added home page and made migrations

--- djangodaisy/accounts/models.py
+++ djangodaisy/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class CustomUser(AbstractUser):
+    # Add any additional fields you want to include in your custom user model
+    # For example:
+    # bio = models.TextField(blank=True)
+    pass
 # Create your models here.
